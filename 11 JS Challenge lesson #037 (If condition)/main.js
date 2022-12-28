73be957a82f5d2cf6787492e307ed3fb74597273
Khaled Radwan <khaledradwan96@gmail.com>
2022-12-28 23:07:11 +0200

    Solved Challenge Lesson 37 (If condition)

---
+++ 11 JS Challenge lesson #037 (If condition)/main.js
@@ -0,0 +1,48 @@
+let a = 10;
+
+if (a < 10) {
+  console.log(10);
+} else if (a >= 10 && a <= 40) {
+  console.log("10 To 40");
+} else if (a > 40) {
+  console.log("> 40");
+} else {
+  console.log("Unknown");
+}
+
+// Write Previous Condition With Ternary If Syntax
+a < 10
+  ? console.log(10)
+  : a >= 10 && a <= 40
+  ? console.log("10 To 40")
+  : a > 40
+  ? console.log("> 40")
+  : console.log("Unknown");
+
+/* ========================================= */
+
+let st = "Elzero Web School";
+
+if ((st.length * 2).toString() === "34") {
+  console.log("Good 01");
+}
+
+// W Position May Change
+if (st.charAt(st.indexOf("W")).toLowerCase() === "w") {
+  console.log("Good 02");
+}
+
+if (st.length !== "string") {
+  console.log("Good 03");
+}
+
+if (typeof Number(st) === "number") {
+  console.log("Good 04");
+}
+
+if (st.split(" ", 1).toString().repeat(2) === "ElzeroElzero") {
+  console.log("Good 05");
+}
+if (st.slice(0, 6).repeat(2) === "ElzeroElzero") {
+  console.log("Good 05");
+}
