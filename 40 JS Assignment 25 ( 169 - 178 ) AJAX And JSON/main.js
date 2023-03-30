159f19c8f10e495d6ab0b0cf21e4bb7ecf0cc0be
Khaled Radwan <khaledradwan96@gmail.com>
2023-03-30 03:19:18 +0200

    Solve JS Assignment 25 ( 169 - 178 ) AJAX And JSON

---
+++ 40 JS Assignment 25 ( 169 - 178 ) AJAX And JSON/main.js
@@ -0,0 +1,69 @@
+// Assignments For Lessons 169 To 178
+
+/* === */ console.log("===> التكليف 01 <==="); /* === */
+// create Json file
+
+/* === */ console.log("===> التكليف 02 & 03 <==="); /* === */
+
+let myReq = new XMLHttpRequest();
+myReq.open("GET", "articles.json");
+myReq.send();
+console.log(myReq);
+
+myReq.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    // التكليف 02
+    console.log(this.responseText);
+    console.log("Data Loaded");
+
+    // التكليف 03
+    let mainData = JSON.parse(this.responseText);
+    console.log(mainData);
+
+    mainData.forEach((articles) => {
+      articles["section"] = "all";
+    });
+    console.log(mainData);
+
+    let updatedData = JSON.stringify(mainData);
+    console.log(updatedData);
+
+    // التكليف 04
+    let datDiv = document.createElement("div");
+    datDiv.id = "data";
+    datDiv.style.cssText=`d`
+
+    for (let i = 0; i < mainData.length; i++) {
+      let div = document.createElement("div");
+      datDiv.appendChild(div);
+
+      // Create Title
+      let title = document.createElement("h2");
+      title.appendChild(document.createTextNode(`Title: ${mainData[i].title}`));
+      div.appendChild(title);
+
+      // Create Article Number
+      let num = document.createElement("p");
+      num.appendChild(document.createTextNode(`Article Number ${i + 1} Body`));
+      div.appendChild(num);
+
+      // Create Author Name
+      let author = document.createElement("p");
+      author.appendChild(
+        document.createTextNode(`Author: ${mainData[i].author}`)
+      );
+      div.appendChild(author);
+
+      // Create Category
+      let category = document.createElement("p");
+      category.appendChild(
+        document.createTextNode(`Category: ${mainData[i].section}`)
+      );
+      div.appendChild(category);
+    }
+
+    document.body.appendChild(datDiv);
+  }
+};
+
+/* === */ console.log("===> التكليف 03 <==="); /* === */
