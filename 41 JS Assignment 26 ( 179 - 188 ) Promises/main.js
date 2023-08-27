f3b11638ae346cfb2956ac515c48aba1229bc462
Khaled Radwan <khaledradwan96@gmail.com>
2023-08-27 21:10:43 +0300

    Solved of Assignment 26 (Promises)

---
+++ 41 JS Assignment 26 ( 179 - 188 ) Promises/main.js
@@ -0,0 +1,58 @@
+// Assignments For Lessons 179 To 188
+
+// التكليف الأول
+const getData = (fileJson) => {
+  return new Promise((resolve, reject) => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.onload = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        resolve(JSON.parse(this.responseText));
+      } else {
+        reject(Error("No Data Found"));
+      }
+    };
+
+    myRequest.open("GET", fileJson);
+    myRequest.send();
+  });
+};
+
+getData("data.json")
+  .then((data) => {
+    data.length = 5;
+    return data;
+  })
+  .then((showData) => {
+    console.log(showData);
+    showData.forEach((data) => {
+      // Create Container Div
+      let div = document.createElement("div");
+
+      // Create Title
+      let title = document.createElement("h3");
+      title.appendChild(document.createTextNode(data.title));
+      div.appendChild(title);
+
+      // Create description
+      let description = document.createElement("p");
+      description.appendChild(document.createTextNode(data.description));
+      div.appendChild(description);
+
+      document.body.appendChild(div);
+    });
+    document.body.appendChild(document.createElement("hr"));
+  })
+  .catch((rej) => console.log(rej));
+
+// التكليف الثاني
+
+fetch("data.json")
+  .then((result) => {
+    let myData = result.json();
+    return myData;
+  })
+  .then((full) => {
+    full.length = 5;
+    console.log(full);
+    return full;
+  });
